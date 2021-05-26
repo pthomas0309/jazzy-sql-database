e6d91dbcf62119fae47321f3ebef12f63802af2e
pthomas0309 <preston.thomas355@gmail.com>
2021-05-26 00:48:17 -0500

    song data generated from songs table DB

--- server/server.js
+++ server/server.js
@@ -31,42 +31,7 @@ pool.on('error', (error) => {
     console.log(error);
 });
 
-const artistList = [ 
-    {
-        name: 'Ella Fitzgerald',
-        birthdate: '04-25-1917'
-    },
-    {
-        name: 'Dave Brubeck',
-        birthdate: '12-06-1920'
-    },       
-    {
-        name: 'Miles Davis',
-        birthdate: '05-26-1926'
-    },
-    {
-        name: 'Esperanza Spalding',
-        birthdate: '10-18-1984'
-    },
-]
-const songList = [
-    {
-        title: 'Take Five',
-        length: '5:24',
-        released: '1959-09-29'
-    },
-    {
-        title: 'So What',
-        length: '9:22',
-        released: '1959-08-17'
-    },
-    {
-        title: 'Black Gold',
-        length: '5:17',
-        released: '2012-02-01'
-    }
-];
-
+// GET && POST
 app.get('/artist', (req, res) => {
     console.log(`In /songs GET`);
     // GET from artist database
@@ -90,7 +55,18 @@ app.post('/artist', (req, res) => {
 
 app.get('/song', (req, res) => {
     console.log(`In /songs GET`);
-    res.send(songList);
+    // GET from artist database
+    let queryText = `SELECT * FROM "songs";`
+    // send the query to DB
+    // make a promise
+    pool.query(queryText)
+        .then((result) => {
+            console.log(result.rows);
+            res.send(result.rows)
+        }).catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        });
 });
 
 app.post('/song', (req, res) => {
